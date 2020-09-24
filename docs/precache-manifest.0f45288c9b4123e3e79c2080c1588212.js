bdd963f3f0205ba3231591aafce0dc35469c4b6b
Uriel <urielbenitez96@hotmail.com>
2020-09-24 15:57:54 -0500

    version 1.0

---
+++ docs/precache-manifest.0f45288c9b4123e3e79c2080c1588212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a74960de3108209953ae400f8e580f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b682db11d53167a6934",
+    "url": "/static/css/main.bcc556aa.chunk.css"
+  },
+  {
+    "revision": "ac8e4437971b52a39111",
+    "url": "/static/js/2.d9f4a947.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d9f4a947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b682db11d53167a6934",
+    "url": "/static/js/main.2e0300ba.chunk.js"
+  },
+  {
+    "revision": "be29b1d0fd333e358891",
+    "url": "/static/js/runtime-main.0cfe70e1.js"
+  }
+]);
